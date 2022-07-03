989d3574e2bd6fdebdaa54b33f48f5a48e5f4ce8
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-07-04 00:59:34 +0300

    completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header({ isDarkMode, onDarkModeClick }) {
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+  );
+}
